2513f756486388f324a9fbc63843d3d5af289db3
TAEWOONG KANG <ktw2378@korea.ac.kr>
2024-02-18 16:42:40 +0900

    REFACTOR : 코드 리팩토링

--- libs/ui/src/lib/header.tsx
+++ libs/ui/src/lib/header.tsx
@@ -2,6 +2,9 @@ import { ReactNode } from 'react';
 import styles from './header.module.css';
 
 export function Header(props: { children: ReactNode }) {
-  return <h1 data-testid="header" className={styles['header']}>{props.children}</h1>;
-
+  return (
+    <h1 data-testid='header' className={styles.header}>
+      {props.children}
+    </h1>
+  );
 }
